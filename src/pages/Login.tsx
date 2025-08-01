f782ddd7a1a94af63e55771d42dd9299f44c21f1
Mark Christian Mallari <mallariftw01@gmail.com>
2025-08-01 16:48:51 +0800

    feat: Add error validation on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,12 +7,20 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
 
     const login = useAuthStore((state) => state.login);
     const navigate = useNavigate();
 
     //login user
     const handleLogin = async () => {
+        setError("")
+
+        if (!/^\S+@\S+\.\S+$/.test(email)) {
+            setError("Invalid email format");
+            return;
+        }
+
         setLoading(true)
         try {
             const user = await loginUser(email, password);
@@ -22,11 +30,12 @@ const Login = () => {
                 navigate(`/${user.role}/dashboard`)
                 setLoading(false)
             } else {
-                alert("Login failed.")
+                setError("Invalid email or password!");
                 setLoading(false)
             }
         } catch (error) {
-            console.log("Login error", error)
+            console.log("Login error", error);
+            setError("Something went wrong. Please try again.");
         } finally {
             setLoading(false)
         }
@@ -38,16 +47,26 @@ const Login = () => {
     }
 
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
+        const value = e.target.value;
+        setEmail(value);
+
+        if (value) {
+            setError("")
+        }
     }
 
     const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
+        const value = e.target.value;
+        setPassword(value);
+
+        if (value) {
+            setError("");
+        }
     }
 
     return (
-        <div className="bg-emerald-50 min-h-screen grid place-items-center">
-            <div className="bg-white shadow-xl px-10 py-14 rounded-md w-full lg:w-1/4  ring-1 ring-inset ring-gray-200">
+        <div className="px-4 lg:px-0 bg-emerald-50  min-h-screen grid place-items-center">
+            <div className=" bg-white shadow-xl px-10 py-14 rounded-md w-full lg:w-1/4  ring-1 ring-inset ring-gray-200">
 
                 <div className="flex flex-col justify-center items-center text-center">
                     <div className="bg-linear-to-r from-emerald-400 to-emerald-500 mb-4 rounded-md">
@@ -57,19 +76,24 @@ const Login = () => {
                     </div>
                     <div>
                         <h2 className="text-xl font-medium">Mallari's Poultry Supply</h2>
-                        <p>Pet Supplies</p>
+                        <p className="text-gray-500">Pet Supplies</p>
                     </div>
                 </div>
                 <div className="mt-10">
                     <label htmlFor="username" className="font-medium">Email</label>
-                    <input type="email" value={email} onChange={handleEmailChange} className="mt-1 input input-md w-full focus:outline-success" placeholder="Enter username" />
+                    <input type="email" value={email} onChange={handleEmailChange} onKeyUp={handleKeyEnterLogin} className="mt-1 input input-md w-full focus:outline-success" placeholder="Enter username" />
                 </div>
                 <div className="mt-4">
                     <label htmlFor="username" className="font-medium">Password</label>
                     <input type="password" value={password} onChange={handlePasswordChange} onKeyUp={handleKeyEnterLogin} className="mt-1 input input-md   w-full focus:outline-success" placeholder="Enter password" />
                 </div>
+                {error && (
+                    <div className="mt-2">
+                        <p className="text-error">{error}</p>
+                    </div>
+                )}
                 <div className="mt-4">
-                    <button onClick={handleLogin} className="btn btn-md bg-linear-to-r from-emerald-400 to-emerald-500 disabled:opacity-50 disabled:bg-none disabled:text-gray-500 disabled:cursor-not-allowed hover:from-emerald-400 hover:to-emerald-600 w-full transition-all ease-in-out" disabled={loading || !email || !password}>{loading ? 'Logging in' : 'Log in'}</button>
+                    <button onClick={handleLogin} className="btn btn-md bg-linear-to-r from-emerald-400 to-emerald-500 disabled:opacity-50 disabled:bg-none disabled:text-gray-500 disabled:cursor-not-allowed hover:from-emerald-400 hover:to-emerald-600 w-full transition-all ease-in-out" disabled={loading || !email || !password}>{loading ? 'Logging in...' : 'Log in'}</button>
                 </div>
             </div>
         </div>

6da4309550a210152c5c59e38e9dde23e07ff9f1
Mark Christian Mallari <mallariftw01@gmail.com>
2025-09-29 21:26:39 +0800

    refactor: Table Header Column

--- src/pages/admin/Users.tsx
+++ src/pages/admin/Users.tsx
@@ -12,10 +12,11 @@ const Users = () => {
                         {/* head */}
                         <thead>
                             <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Job</th>
-                                <th>Favorite Color</th>
+                                <th>ID</th>
+                                <th>Full Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Active</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -25,20 +26,23 @@ const Users = () => {
                                 <td>Cy Ganderton</td>
                                 <td>Quality Control Specialist</td>
                                 <td>Blue</td>
+                                <td>Blue</td>
                             </tr>
                             {/* row 2 */}
                             <tr>
-                                <th>2</th>
-                                <td>Hart Hagerty</td>
-                                <td>Desktop Support Technician</td>
-                                <td>Purple</td>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                                <td>Blue</td>
                             </tr>
                             {/* row 3 */}
                             <tr>
-                                <th>3</th>
-                                <td>Brice Swyre</td>
-                                <td>Tax Accountant</td>
-                                <td>Red</td>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                                <td>Blue</td>
                             </tr>
                         </tbody>
                     </table>

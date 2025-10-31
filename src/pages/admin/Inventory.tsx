9320f3ea1a345c5677fe35bbcfcd86a2302e8da2
Mark Christian Mallari <mallariftw01@gmail.com>
2025-11-01 01:35:24 +0800

    feat: Add stock button

--- src/pages/admin/Inventory.tsx
+++ src/pages/admin/Inventory.tsx
@@ -1,4 +1,4 @@
-import { Plus } from "lucide-react"
+import { Box, Plus } from "lucide-react"
 import { useRef, useState } from "react"
 
 const Inventory = () => {
@@ -30,11 +30,13 @@ const Inventory = () => {
         </button>
         <dialog ref={modalRef} className="modal">
           <div className="modal-box">
-            <h3 className="font-bold text-lg">{mode == "add" ? "Add" : "Update"}</h3>
+            <h3 className="font-bold text-lg mb-2">{mode == "add" ? "Add Product" : "Update Product"}</h3>
+            <label htmlFor="productName">Product Name:</label>
+            <input type="text" className="block input w-full" />
 
-            <p className="py-4">Press ESC key or click the button below to close</p>
             <div className="modal-action">
               <form method="dialog">
+
                 <button className="btn">Close</button>
               </form>
             </div>
@@ -66,6 +68,9 @@ const Inventory = () => {
               <td>2025-10-10</td>
               <td>
                 <div className="inline-flex gap-1 font-medium">
+                  <button className="bg-amber-500 rounded-sm px-4 py-1 cursor-pointer">
+                    <Box className="size-5" />
+                  </button>
                   <button onClick={() => openModal("update")} className="bg-blue-400 rounded-sm px-4 py-1 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                       <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
@@ -76,6 +81,7 @@ const Inventory = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
                   </button>
+
                 </div>
               </td>
             </tr>

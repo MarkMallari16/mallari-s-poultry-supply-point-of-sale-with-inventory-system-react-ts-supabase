83a3a2a35052bb030b373960613a00e813b86bbf
Mark Christian Mallari <mallariftw01@gmail.com>
2025-11-07 21:15:48 +0800

    refactor: Remove unecessary comments

--- src/pages/admin/Inventory.tsx
+++ src/pages/admin/Inventory.tsx
@@ -69,10 +69,8 @@ const Inventory = () => {
           </div>
         </dialog>
       </div>
-      {/*Table*/}
       <div className="mt-6 overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
               <th></th>

2aa93bb216dbcef43c08ad252f562c176541af51
Mark Christian Mallari <mallariftw01@gmail.com>
2025-11-16 13:33:12 +0800

    refactor: change ux of products and users

--- src/pages/admin/Users.tsx
+++ src/pages/admin/Users.tsx
@@ -119,15 +119,15 @@ const Users = () => {
             </dialog>
             <div className="flex items-center justify-between">
                 <div>
-                    <h1 className="text-xl font-bold">Staffs</h1>
-                    <span className="text-gray-500">Manage your staffs</span>
+                    <h1 className="text-xl font-bold">Staff</h1>
+                    <span className="text-gray-500">Manage your staff members.</span>
                 </div>
 
                 <button className="btn bg-emerald-500" onClick={() => openModal("add")}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                     </svg>
-                    Add users
+                    Add staff
                 </button>
             </div>
 
